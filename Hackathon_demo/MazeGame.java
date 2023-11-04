c8cc995d3408f39297d910f8f13e5d807035bf51
tsinghdhesi <tsinghdhesi@colgate.edu>
2023-11-04 04:21:45 -0400

    grid initialized

--- Hackathon_demo/MazeGame.java
+++ Hackathon_demo/MazeGame.java
@@ -2,12 +2,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import javafx.application.Application;
-
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 
 import java.util.*;
 
@@ -19,8 +19,10 @@ public class MazeGame extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("MazeGame");
         primaryStage.setMaximized(true);
-        VBox layout = new VBox();
-        ImageView[][] iv = new ImageView[9][18];//initialising grid of images
+        GridPane layout = new GridPane();
+        ImageView[][] iv = new ImageView[9][19];//initialising grid of images
+        // System.out.println("Primary Screen Width: " + screenWidth + " pixels");
+        // System.out.println("Primary Screen Height: " + screenHeight + " pixels");
         ImageView img; //dummy variable to hold individual image
         FileInputStream inputstream = null;
         try {
@@ -29,12 +31,31 @@ public class MazeGame extends Application {
             System.out.println("File not found");
         } 
         Image drt = new Image(inputstream); 
-        for(int i =0; i < 9; i++){
-            for(int j = 0; j < 18;j++){
-                img = iv[i][j];
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 19;j++){
+                img = new ImageView();
                 img.setImage(drt);
-                img.setLayoutX(i);
-                layout.getChildren().add(img);
+                img.setFitHeight(100);
+                img.setFitWidth(100);                
+                iv[i][j] = img;
+                layout.add(img, j, i);
+            }
+        }try {
+            inputstream = new FileInputStream("Hackathon_demo\\rock.jpg");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } 
+        Image rck = new Image(inputstream);
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 19;j++){
+                img = iv[2*i+1][j];
+                img.setImage(rck);
+            }
+        }
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9;j++){
+                img = iv[i][2*j+1];
+                img.setImage(rck);
             }
         }
         // Create a label
@@ -46,10 +67,10 @@ public class MazeGame extends Application {
 
         // Create a layout and add the label and button to it
          // 10 is the spacing between elements
-        layout.getChildren().addAll(label, button);
+        //layout.getChildren().addAll(label, button);
 
         // Create a scene and add the layout to it
-        Scene scene = new Scene(layout, 300, 200); // Width and height of the scene
+        Scene scene = new Scene(layout, 1920, 1080); // Width and height of the scene
 
         // Set the scene for the primary stage
         primaryStage.setScene(scene);
