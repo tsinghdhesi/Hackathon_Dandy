025657b83fc12b7aa2093f50945e60f3a54ab846
tsinghdhesi <tsinghdhesi@colgate.edu>
2023-11-04 02:41:14 -0400

    work in progress

--- Hackathon_demo/MazeGame.java
+++ Hackathon_demo/MazeGame.java
@@ -1,19 +1,42 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
+
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.*;
 
 public class MazeGame extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX Example");
-
+        primaryStage.setTitle("MazeGame");
+        primaryStage.setMaximized(true);
+        VBox layout = new VBox();
+        ImageView[][] iv = new ImageView[9][18];//initialising grid of images
+        ImageView img; //dummy variable to hold individual image
+        FileInputStream inputstream = null;
+        try {
+            inputstream = new FileInputStream("Hackathon_demo\\dirt.jpg");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } 
+        Image drt = new Image(inputstream); 
+        for(int i =0; i < 9; i++){
+            for(int j = 0; j < 18;j++){
+                img = iv[i][j];
+                img.setImage(drt);
+                img.setLayoutX(i);
+                layout.getChildren().add(img);
+            }
+        }
         // Create a label
         Label label = new Label("Hello, JavaFX!");
 
@@ -22,7 +45,7 @@ public class MazeGame extends Application {
         button.setOnAction(e -> label.setText("Button Clicked!"));
 
         // Create a layout and add the label and button to it
-        VBox layout = new VBox(10); // 10 is the spacing between elements
+         // 10 is the spacing between elements
         layout.getChildren().addAll(label, button);
 
         // Create a scene and add the layout to it
@@ -31,7 +54,28 @@ public class MazeGame extends Application {
         // Set the scene for the primary stage
         primaryStage.setScene(scene);
 
+        
         // Show the stage
         primaryStage.show();
+        
     }
+    
+    public void Kruskals(ImageView iv, ArrayList<ArrayList<HashSet<ImageView>>> pathTiles, LinkedList<ImageView> edgeTiles){
+        // 1. grid of images
+
+        // 2. set of sets for path tile (2d arr?)
+
+        //3. list of edges 
+
+        //
+        //ImageView tmp = edgeTiles.pop();
+
+        //
+
+        
+
+        
+    }
+        
+    
 }
